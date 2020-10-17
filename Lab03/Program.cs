f9806fd6b81c23ef70c68e19eab0826587703cff
ivanchikov-alex <mr.ivanchikov@yandex.by>
2020-10-17 10:52:29 +0300

    16.10.2020

--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -2,11 +2,68 @@
 
 namespace Lab03
 {
+    public partial class Airline
+    {
+        public enum WeekDays { monday = 1, tuestay, wednesday, thirsday, friday, saturday, sunday }
+        int flightNum { get; set; }
+        WeekDays day { get; set; }
+        string destination { get; set; }
+        const string departPoint = "Гуанчжоу";
+        string departTime { get; set; }
+        string planeType
+        {
+            set
+            {
+                if (value.ToLower() == "почтовый" || value.ToLower() == "пассажирский")
+                    planeType = value;
+                else Console.WriteLine("Данного типа нет в наличии");
+            }
+            get
+            {
+                return planeType;
+            }
+        }
+        static int listSize { get; set; }
+    }
+    public partial class Airline
+    { 
+        static Airline()
+        {
+            listSize = 0;
+        }
+        private Airline(){ }
+
+        public Airline(WeekDays day)
+        {
+            listSize++;
+            flightNum++;
+            this.day = day;
+            destination = "Минск";
+            departTime = "0:00";
+            planeType = "Почтовый";
+        }
+
+        public Airline(int flightNum, WeekDays day, string destination, string departTime, string planeType = "Пассажирский")
+        {
+            listSize++;
+            this.flightNum = flightNum;
+            this.day = day;
+            this.destination = destination;
+            this.departTime = departTime;
+            this.planeType = planeType;
+        }
+
+        public static void ClassInfo()
+        {
+            Console.WriteLine($"{listSize}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Airline flight1 = new Airline(Airline.WeekDays.sunday);
+            Airline.ClassInfo();
         }
     }
 }
