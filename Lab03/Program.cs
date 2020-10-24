e0048b7ea8236333e0857fb63d13598ff675fbb7
ivanchikov-alex <mr.ivanchikov@yandex.by>
2020-10-24 13:02:07 +0300

    24.10.2020

--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -5,42 +5,54 @@ namespace Lab03
     public partial class Airline
     {
         public enum WeekDays { monday = 1, tuestay, wednesday, thirsday, friday, saturday, sunday }
-        int flightNum { get; set; }
-        WeekDays day { get; set; }
-        string destination { get; set; }
+        int flightNum;
+        public WeekDays day;
+        public string destination;
         const string departPoint = "Гуанчжоу";
-        string departTime { get; set; }
-        string planeType
+        string departTime;
+        string planeType;
+        static int listSize;
+        public readonly int id;
+        public string DepartTime { get; set; }
+        public string ListSize { get; set; }
+        public string PlaneType
         {
             set
             {
                 if (value.ToLower() == "почтовый" || value.ToLower() == "пассажирский")
-                    planeType = value;
-                else Console.WriteLine("Данного типа нет в наличии");
+                    PlaneType = value;
+                else PlaneType = null;
             }
             get
             {
-                return planeType;
+                return PlaneType;
             }
         }
-        static int listSize { get; set; }
     }
     public partial class Airline
-    { 
+    {
         static Airline()
         {
             listSize = 0;
         }
-        private Airline(){ }
+        private Airline()
+        {
+            listSize++;
+            flightNum++;
+            destination = "Минск";
+            departTime = "0:00";
+            planeType = "Почтовый";
+            id = this.GetHashCode();
+        }
 
         public Airline(WeekDays day)
         {
             listSize++;
             flightNum++;
-            this.day = day;
             destination = "Минск";
             departTime = "0:00";
             planeType = "Почтовый";
+            this.day = day;
         }
 
         public Airline(int flightNum, WeekDays day, string destination, string departTime, string planeType = "Пассажирский")
@@ -57,13 +69,69 @@ namespace Lab03
         {
             Console.WriteLine($"{listSize}");
         }
+
+        public void ChangeDestination(out string oldDest, ref string newDest)
+        {
+            oldDest = newDest;
+        }
+        public bool Equals(Airline flight)
+        {
+            if (this.GetHashCode() == flight.GetHashCode())
+                return true;
+            else return false;
+        }
+
+        new public string ToString()
+        {
+            return this.day + " " + destination + " " + departTime + " " + flightNum;
+        }
+
+        new public int GetHashCode()
+        {
+            return this.flightNum.GetHashCode() + destination.GetHashCode();
+        }
+
+        public int GetID()
+        {
+            Airline flight = new Airline();
+            if (this.flightNum == 0 && destination == null)
+                return flight.id;
+            else return this.GetHashCode();
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            var anonType = new { destination = "Канто", departTime = "10:00" };
             Airline flight1 = new Airline(Airline.WeekDays.sunday);
+            Airline flight2 = new Airline(2, Airline.WeekDays.friday, "Минск", "20:10");
+            Console.WriteLine(flight2.ToString());
+            Console.WriteLine(flight1.GetID());
             Airline.ClassInfo();
+            Console.WriteLine(flight1.GetHashCode());
+            Console.WriteLine("Новый пункт назначения");
+            string newdest = Console.ReadLine();
+            flight1.ChangeDestination(out flight1.destination, ref newdest);
+            Console.WriteLine(flight1.Equals(flight2));
+            Console.WriteLine(flight1.GetType());
+
+            Airline[] flights = new Airline[5];
+            for (int i = 0; i < 5; i++)
+                flights[i] = new Airline((Airline.WeekDays)i+1);
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+                if (flights[i].destination == "Минск")
+                    Console.WriteLine(flights[i].ToString());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+                if (flights[i].day == (Airline.WeekDays)3)
+                    Console.WriteLine($"{flights[i].ToString()}");
+
         }
     }
 }
